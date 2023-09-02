9d9f5ff06be5b956211b2e22ff18ab300d442ee1
sd <leowest1921@gmail.com>
2023-09-02 09:45:16 +0800

    add

--- shortGPT/editing_framework/core_editing_engine.py
+++ shortGPT/editing_framework/core_editing_engine.py
@@ -194,7 +194,7 @@ class CoreEditingEngine:
     def process_video_asset(self, asset: Dict[str, Any]) -> VideoFileClip:
         params = {
             'filename': handle_path(asset['parameters']['url']),
-            'duration': asset['parameters']['duration'] if 'duration' in asset['parameters'] else 15
+            #'duration': asset['parameters']['duration'] if 'duration' in asset['parameters'] else 15
         }
         if 'audio' in asset['parameters']:
             params['audio'] = asset['parameters']['audio']
